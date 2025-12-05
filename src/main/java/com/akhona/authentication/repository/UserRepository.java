fe067a5287d332c60184867084fd4ddc531f88ce
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-12-05 20:30:00 +0200

    Feat - Create User repository to find user by email

---
+++ src/main/java/com/akhona/authentication/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.akhona.authentication.repository;
+
+import com.akhona.authentication.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
