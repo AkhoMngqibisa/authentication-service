48ae3dc4f39db02e4e3f6e830abcec45799f3dca
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-12-07 19:30:00 +0200

    Feat - Update AuthResponse model with proper data

--- src/main/java/com/akhona/authentication/dto/AuthResponse.java
+++ src/main/java/com/akhona/authentication/dto/AuthResponse.java
@@ -1,4 +1,20 @@
 package com.akhona.authentication.dto;
 
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
+    private Long userId;
+    private String email;
+    private String role;
+
+    private String accessToken;
+    private String refreshToken;
+
+    private String tokenType;
+
+    private long expiresIn;
 }
