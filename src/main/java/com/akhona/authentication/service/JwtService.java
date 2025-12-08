c088fb2d1c2e79a5dd8152421090f58a4b81f50e
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-12-08 19:15:00 +0200

    Feat - Change the secret key algorithm

--- src/main/java/com/akhona/authentication/service/JwtService.java
+++ src/main/java/com/akhona/authentication/service/JwtService.java
@@ -1,22 +1,25 @@
 package com.akhona.authentication.service;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    private final String secret = "verysecretkey";
+    private final String secret = "my-super-long-secret-key-that-is-at-least-32-bytes!";
+    private final SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
 
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-                .signWith(SignatureAlgorithm.HS256, secret)
+                .signWith(key)
                 .compact();
     }
 }
