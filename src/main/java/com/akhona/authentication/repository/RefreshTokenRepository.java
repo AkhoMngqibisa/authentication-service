365dd4d3f46f0a695bd7aede8dcbc4c096f14497
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-12-09 18:00:00 +0200

    Feat - Create a refresh token repository with findbyToken and deleteByUser

---
+++ src/main/java/com/akhona/authentication/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package com.akhona.authentication.repository;
+
+import com.akhona.authentication.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByUser(User user);
+}
