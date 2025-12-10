2d239482a6977b3ddf516ecdd0587eef474bc303
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-12-10 18:00:00 +0200

    Feat - Bridges Spring Security with the database.

---
+++ src/main/java/com/akhona/authentication/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.akhona.authentication.security;
+
+import com.akhona.authentication.entity.User;
+import com.akhona.authentication.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(user);
+    }
+}
