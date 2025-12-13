4f585dcf36197e8701fffdad1b7b6e04f0ec6e95
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-12-13 20:00:00 +0200

    Config - Enable Swagger in Security

--- src/main/java/com/akhona/authentication/config/SecurityConfig.java
+++ src/main/java/com/akhona/authentication/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**")
+                        .requestMatchers("/auth/**", "/swagger-ui/**", "/swagger-ui.html","/v3/api-docs/**")
                         .permitAll()  // public endpoints
                         .anyRequest()
                         .authenticated()) // all others require authentication
